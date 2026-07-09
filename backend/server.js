0dce17081f8ba991c892bc433e4472543cdc9b3a
Anurag Kumar <ak7546824@gmail.com>
2026-07-10 00:48:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ import googleAuthRouter from './routes/googleAuth.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB connection + server start
-mongoose.connect('mongodb://127.0.0.1:27017/User')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
